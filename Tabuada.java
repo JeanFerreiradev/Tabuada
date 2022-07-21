7bb35cad0d9d75595e2db8caa335aa057d243b05
Jean WIlson Ferreira <100369102+JeanWf1213@users.noreply.github.com>
2022-07-20 21:43:32 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,30 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class Tabuada {
+	static Scanner console = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int num = 0;
+		int multiplicador = 0;
+		System.out.println("Digite um número (Obs: Número > 2):");
+		num = console.nextInt();
+		while (num <= 2 || num >= 1000) {
+			System.out.println("Número inválido, digite novamente:");
+			num = console.nextInt();
+		}
+		for (multiplicador = 1; multiplicador <= 10; multiplicador++) {
+			System.out.println(multiplicador + " x " + num + " = " + multiplicador * num);
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
