ed56b455c8777923372c116ef2b72ef7a3a40c50
Jean WIlson Ferreira <100369102+JeanWf1213@users.noreply.github.com>
2022-07-20 21:46:08 -0300

    Update Tabuada.java

--- Tabuada.java
+++ Tabuada.java
@@ -8,10 +8,10 @@ public class Tabuada {
 	public static void main(String[] args) {
 		int num = 0;
 		int multiplicador = 0;
-		System.out.println("Digite um número (Obs: Número > 2):");
+		System.out.println("Digite um nÃºmero (Obs: 2 < NÃºmero < 1000):");
 		num = console.nextInt();
 		while (num <= 2 || num >= 1000) {
-			System.out.println("Número inválido, digite novamente:");
+			System.out.println("NÃºmero invÃ¡lido, digite novamente:");
 			num = console.nextInt();
 		}
 		for (multiplicador = 1; multiplicador <= 10; multiplicador++) {
